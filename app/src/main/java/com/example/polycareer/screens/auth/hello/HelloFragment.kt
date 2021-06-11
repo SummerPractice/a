8c7a3adc6c6b13b591e3db1169226b8dea73f3d2
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-06-11 18:03:14 +0300

    [POL-4] Сделал переход на страницу регистрации

--- app/src/main/java/com/example/polycareer/screens/auth/hello/HelloFragment.kt
+++ app/src/main/java/com/example/polycareer/screens/auth/hello/HelloFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.polycareer.R
 
-class HelloFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class HelloFragment : Fragment(), View.OnClickListener {
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment__auth__hello, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = findNavController(this)
+        val button = view.findViewById<Button>(R.id.fragment__auth__hello_start_btn)
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        navController.navigate(R.id.action_helloFragment_to_signUpFragment)
     }
 
     companion object {
