4799acb54cb72bae7d4c1e99b9f637b85c653d1f
mishka361 <elotomp@gmail.com>
2021-06-24 15:08:29 +0300

    [POL-33] Добавил экран со списком результатов

---
+++ app/src/main/java/com/example/polycareer/screens/main/old_reults/OldResultsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.polycareer.screens.main.old_reults
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.polycareer.R
+
+class OldResultsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_old_results, container, false)
+    }
+}
