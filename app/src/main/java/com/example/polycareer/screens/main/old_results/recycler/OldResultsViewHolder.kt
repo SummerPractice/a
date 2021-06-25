fa01953d7023ad02b328f879dbf5b2a994abf0fa
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-06-25 16:51:49 +0300

    [POL-33] Сделал красивое отображение времени и правильное отборажение текста

--- app/src/main/java/com/example/polycareer/screens/main/old_results/recycler/OldResultsViewHolder.kt
+++ app/src/main/java/com/example/polycareer/screens/main/old_results/recycler/OldResultsViewHolder.kt
@@ -6,15 +6,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.polycareer.R
 import com.example.polycareer.domain.model.UserResultInfo
 
-class OldResultsViewHolder(itemView: View, val listener : OldResultsAdapter.OnResultItemClickListener) :RecyclerView.ViewHolder(itemView) {
+class OldResultsViewHolder(itemView: View, private val listener : OldResultsAdapter.OnResultItemClickListener) :RecyclerView.ViewHolder(itemView) {
     private var number: TextView = itemView.findViewById(R.id.dir_item__tv_num1)
     private val title: TextView = itemView.findViewById(R.id.dir_item__tv_dest)
     private var dateOfTry: TextView = itemView.findViewById(R.id.dir_item__tv_descr)
 
     fun bind(result: UserResultInfo) {
         number.text = result.tryNumber.toString()
-        title.text = "Попытка №${result.tryNumber}"
-        dateOfTry.text = result.date.toString()
+        title.text = itemView.context.getString(R.string.try_title, result.tryNumber)
+        dateOfTry.text = result.date
         itemView.setOnClickListener { listener.onItemClick(result.tryNumber) }
     }
 }
